89bd2eb3dd660c2d6bac6e70e8f96e873b5a7083
Firmansyah <147714843+Manzzzx@users.noreply.github.com>
2024-11-11 18:46:53 +0700

    memindahkan array $post ke class

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,29 @@
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
+class Post 
+{
+    public static function all()
+    {
+        return [
+            [
+                'id' => '1',
+                'slug' => 'judul-artikel-1',
+                'title' => 'Judul Artikel 1',
+                'author' => 'Firmansyah',
+                'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non fuga, debitis numquam esse nihil temporibus harum aliquam expedita delectus illum?'
+            ],
+            [
+                'id' => '2',
+                'slug' => 'judul-artikel-2',
+                'title' => 'Judul Artikel 2',
+                'author' => 'Firmansyah',
+                'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit voluptatibus laboriosam hic, autem aperiam assumenda?'
+            ]
+            ];
+    }
+}
+
 Route::get('/', function () {
     return view('home', ['title' => 'Home']);
 });
@@ -12,22 +35,7 @@ Route::get('/about', function () {
 });
 
 Route::get('/posts', function () {
-    return view('posts', ['title' => 'Blog', 'posts'=> [
-        [
-            'id' => '1',
-            'slug' => 'judul-artikel-1',
-            'title' => 'Judul Artikel 1',
-            'author' => 'Firmansyah',
-            'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non fuga, debitis numquam esse nihil temporibus harum aliquam expedita delectus illum?'
-        ],
-        [
-            'id' => '2',
-            'slug' => 'judul-artikel-2',
-            'title' => 'Judul Artikel 2',
-            'author' => 'Firmansyah',
-            'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit voluptatibus laboriosam hic, autem aperiam assumenda?'
-        ]
-    ]]);
+    return view('posts', ['title' => 'Blog', 'posts'=> Post::all()]);
 });
 
 Route::get('/posts/{slug}', function ($slug) {
