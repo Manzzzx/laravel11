33879a5977fb9939d5fbb084a83c881e0b4ea4dc
Firmansyah <147714843+Manzzzx@users.noreply.github.com>
2024-11-11 18:48:46 +0700

    memperbaiki halaman single post

--- routes/web.php
+++ routes/web.php
@@ -39,24 +39,9 @@ Route::get('/posts', function () {
 });
 
 Route::get('/posts/{slug}', function ($slug) {
-    $posts = [
-        [
-            'id' => '1',
-            'slug'=> 'judul-artikel-1',
-            'title' => 'Judul Artikel 1',
-            'author' => 'Firmansyah',
-            'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non fuga, debitis numquam esse nihil temporibus harum aliquam expedita delectus illum?'
-        ],
-        [
-            'id' => '2',
-            'slug'=> 'judul-artikel-2',
-            'title' => 'Judul Artikel 2',
-            'author' => 'Firmansyah',
-            'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit voluptatibus laboriosam hic, autem aperiam assumenda?'
-        ]
-        ];
+    
 
-        $post = Arr::first($posts, function ($post) use ($slug) {
+        $post = Arr::first(Post::all(), function ($post) use ($slug) {
             return $post['slug'] == $slug;
         });
         
