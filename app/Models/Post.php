25fc21420a91194e6598a2c4fb0f9b6b2079326d
Firmansyah <147714843+Manzzzx@users.noreply.github.com>
2024-11-13 12:28:57 +0700

    eloquent ORM & post model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,38 +2,11 @@
 
 namespace App\Models;
 
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Post 
+class Post extends Model
 {
-    public static function all()
-    {
-        return [
-            [
-                'id' => '1',
-                'slug' => 'judul-artikel-1',
-                'title' => 'Judul Artikel 1',
-                'author' => 'Firmansyah',
-                'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non fuga, debitis numquam esse nihil temporibus harum aliquam expedita delectus illum?'
-            ],
-            [
-                'id' => '2',
-                'slug' => 'judul-artikel-2',
-                'title' => 'Judul Artikel 2',
-                'author' => 'Firmansyah',
-                'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit voluptatibus laboriosam hic, autem aperiam assumenda?'
-            ]
-            ];
-    }
-
-
-    public static function find($slug): array {
-        $post = Arr::first(static::all(), fn ($post) => $post['slug'] == $slug);
-        
-        if (!$post) {
-            abort(404);
-        }
-
-        return $post;
-    }
+    use HasFactory;
+    protected $fillable = ['title', 'author', 'slug', 'body'];
 }
