aa3ba6084defcc1fe66c4bab3edd6b901f92f388
Firmansyah <147714843+Manzzzx@users.noreply.github.com>
2024-11-13 15:23:34 +0700

    update

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -9,7 +9,7 @@
             <div class="text-base text-gray-500">
                 <a href="#">{{ $post['author'] }}</a> | {{ $post->created_at->diffForHumans() }}
             </div>
-            <p class="my-4 font-light">{{ Str:: limit($post['body'], 100) }}</p>
+            <p class="my-4 font-light">{{ Str:: limit($post['body'], 200) }}</p>
             <a href="/posts/{{ $post['slug'] }}" class="font-medium text-blue-500 hover:underline">Read more &raquo;</a>
         </article>
     @endforeach
